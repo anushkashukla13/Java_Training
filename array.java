1635d3d256010b726be7b88e37321e3ab5c00229
anushkashukla13 <anushkashukla1331@gmail.com>
2026-03-17 23:46:13 +0530

    next push

--- array.java
+++ array.java
@@ -283,8 +283,49 @@ public class array {
         //     System.out.println(arr[i]);
         // }
 
+        // median of two sorted array
+        // int arr1[]={1,3,5};
+        // int arr2[]={2,4,6};
+        // int arr3[]=new int[arr1.length+arr2.length];
+        // int i=0,j=0,k=0;
+        // while(i<arr1.length && j<arr2.length)
+        // {
+        //     if(arr1[i]<arr2[j])
+        //     {
+        //         arr3[k]=arr1[i];
+        //         i++;
+        //         k++;
+        //     }
+        //     else{
+        //         arr3[k]=arr2[j];
+        //         j++;
+        //         k++;
+        //     }
+        // }
+        // while(i<arr1.length)
+        // {
+        //     arr3[k]=arr1[i];
+        //     i++;
+        //     k++;
+        // }
+        // while(j<arr2.length)
+        // {
+        //     arr3[k]=arr2[j];
+        //     j++;
+        //     k++;
+        // }
         
+        // int median=0;
+        // if(arr3.length%2==0)
+        // {
+        //     median=(arr3[arr3.length/2]+arr3[arr3.length/2-1])/2;
+        // }
+        // else{
+        //     median=arr3[arr3.length/2];
+        // }
+        // System.out.println(median);
 
+        
 
     }
 }
--- question1.java
+++ question1.java
@@ -1,13 +1,13 @@
 public class question1 {
     public static void main(String[] args) {
-        // int a=123;
-        // int sum=0;
-        // while(a>0)
-        // {
-        //     sum+=a%10;
-        //     a/=10;
-        // }
-        // System.out.println(sum);
+    //     int a=123;
+    //     int sum=0;
+    //     while(a>0)
+    //     {
+    //         sum+=a%10;
+    //         a/=10;
+    //     }
+    //     System.out.println(sum);
     
 
     //  int a = 123;
@@ -41,14 +41,44 @@ public class question1 {
     // }
 
     // bitwise
-//     int a=123;
-//     if((a&1)==0){
-//         System.out.println("even");
-//     }
-//     else{
-//         System.out.println("odd");
-//     }
+    // int a=123;
+    // if((a&1)==0){
+    //     System.out.println("even");
+    // }
+    // else{
+    //     System.out.println("odd");
+    // }
+
+    
+    // Count vowels, consonants, digits, spaces in a string
+    String s1="anushka 123";
+    int vowels=0;
+    int consonants=0;
+    int digits=0;
+    int spaces=0;
+    for(int i=0;i<s1.length();i++){
+        char ch=s1.charAt(i);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            vowels++;
+        }
+        else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+            consonants++;
+        }
+        else if(ch>='0'&&ch<='9'){
+            digits++;
+        }
+        else if(ch==' '){
+            spaces++;
+        }
 
 
-    }    
+
+    }
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
+    System.out.println("Digits: " + digits);
+    System.out.println("Spaces: " + spaces);
 }
+
+}
+
