7c7c04b6bedebdff84e85c93b52a8c61ba7298e1
anushkashukla13 <anushkashukla1331@gmail.com>
2026-03-23 23:18:55 +0530

    next push

--- dsa.java
+++ dsa.java
@@ -1,35 +1,39 @@
+import java.util.LinkedList;
+
 // Trapping Rain Water problem
-public class dsa {
-    public static void main(String[] args) {
-        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(trap(height));
-    }
 
-    public static int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = 0;
-        int rightMax = 0;
-        int trappedWater = 0;
+// public class dsa {
+//     public static void main(String[] args) {
+//         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+//         System.out.println(trap(height));
+//     }
 
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    trappedWater += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    trappedWater += rightMax - height[right];
-                }
-                right--;
-            }
-        }
+//     public static int trap(int[] height) {
+//         int left = 0;
+//         int right = height.length - 1;
+//         int leftMax = 0;
+//         int rightMax = 0;
+//         int trappedWater = 0;
 
-        return trappedWater;
-    }
-}
+//         while (left < right) {
+//             if (height[left] < height[right]) {
+//                 if (height[left] >= leftMax) {
+//                     leftMax = height[left];
+//                 } else {
+//                     trappedWater += leftMax - height[left];
+//                 }
+//                 left++;
+//             } else {
+//                 if (height[right] >= rightMax) {
+//                     rightMax = height[right];
+//                 } else {
+//                     trappedWater += rightMax - height[right];
+//                 }
+//                 right--;
+//             }
+//         }
+
+//         return trappedWater;
+//     }
+
+   
