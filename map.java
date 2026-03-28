3500556e8660104a433e169e2caa5d11c660f663
anushkashukla13 <anushkashukla1331@gmail.com>
2026-03-28 23:28:02 +0530

    next push

--- map.java
+++ map.java
@@ -38,25 +38,43 @@ import java.util.*;
 
 public class map {
     public static void main(String[] args) {
-        String str = "aabbcdeff";
+    //     String str = "aabbcdeff";
 
-        HashMap<Character, Integer> map = new HashMap<>();
+    //     HashMap<Character, Integer> map = new HashMap<>();
 
-        // Step 1: Count frequency
-        for (char ch : str.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
+    //     // Step 1: Count frequency
+    //     for (char ch : str.toCharArray()) {
+    //         map.put(ch, map.getOrDefault(ch, 0) + 1);
+    //     }
 
-        // Step 2: Find first non-repeating
-        for (char ch : str.toCharArray()) {
-            if (map.get(ch) == 1) {
-                System.out.println("First non-repeating character: " + ch);
+    //     // Step 2: Find first non-repeating
+    //     for (char ch : str.toCharArray()) {
+    //         if (map.get(ch) == 1) {
+    //             System.out.println("First non-repeating character: " + ch);
+    //             return;
+    //         }
+    //     }
+
+
+    //     System.out.println("No non-repeating character found");
+
+
+    // Two Sum
+     int arr[] = {2, 7, 11, 15};
+        int target = 9;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                System.out.println(map.get(complement) + " " + i);
                 return;
             }
+
+            map.put(arr[i], i);
         }
-
-
-        System.out.println("No non-repeating character found");
     }
 
 
