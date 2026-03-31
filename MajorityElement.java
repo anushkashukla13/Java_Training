42db0edd6338a1736d021f4a69e96ff509a7a26e
anushkashukla13 <anushkashukla1331@gmail.com>
2026-03-31 23:16:30 +0530

    next push

---
+++ MajorityElement.java
@@ -0,0 +1,37 @@
+// Move Zeros to End
+// public class MoveZeros {
+//     public static void main(String[] args) {
+        // int[] arr = {0, 1, 0, 3, 12};
+        // int index = 0;
+
+        // for (int num : arr) {
+        //     if (num != 0) {
+        //         arr[index++] = num;
+        //     }
+        // }
+
+        // while (index < arr.length) {
+        //     arr[index++] = 0;
+        // }
+
+        // for (int num : arr)
+        //     System.out.print(num + " ");
+
+        // Majority Element
+//         public class MajorityElement {
+//     public static void main(String[] args) {
+//         int[] arr = {2,2,1,1,2,2,2};
+
+//         int count = 0, candidate = 0;
+
+//         for (int num : arr) {
+//             if (count == 0) {
+//                 candidate = num;
+//             }
+
+//             count += (num == candidate) ? 1 : -1;
+//         }
+
+//         System.out.println("Majority Element: " + candidate);
+//     }
+// }
