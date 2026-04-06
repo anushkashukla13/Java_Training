273e9e2512a1aa6e5f1d8645938a5c859a06460a
anushkashukla13 <anushkashukla1331@gmail.com>
2026-04-06 23:49:02 +0530

    next push

--- Interface.java
+++ Interface.java
@@ -1,46 +1,73 @@
 // Create an interface Shape with a method area().
 
     
-interface Shape {
-    void area();  // abstract method
+// interface Shape {
+//     void area();  // abstract method
+// }
+
+// // Circle class implementing Shape
+// class Circle implements Shape {
+//     double radius;
+
+//     Circle(double radius) {
+//         this.radius = radius;
+//     }
+
+//     public void area() {
+//         double result = Math.PI * radius * radius;
+//         System.out.println("Area of Circle: " + result);
+//     }
+// }
+
+// // Rectangle class implementing Shape
+// class Rectangle implements Shape {
+//     double length, width;
+
+//     Rectangle(double length, double width) {
+//         this.length = length;
+//         this.width = width;
+//     }
+
+//     public void area() {
+//         double result = length * width;
+//         System.out.println("Area of Rectangle: " + result);
+//     }
+// }
+
+// // Main class
+// public class Interface {
+//     public static void main(String[] args) {
+//         Shape s1 = new Circle(5);
+//         Shape s2 = new Rectangle(4, 6);
+
+//         s1.area();
+//         s2.area();
+//     }
+// }
+
+// Create two interfaces:
+interface Printable {
+    void print();
 }
 
-// Circle class implementing Shape
-class Circle implements Shape {
-    double radius;
+interface Showable {
+    void show();
+}
 
-    Circle(double radius) {
-        this.radius = radius;
+class Demo implements Printable, Showable {
+    public void print() {
+        System.out.println("Printing...");
     }
 
-    public void area() {
-        double result = Math.PI * radius * radius;
-        System.out.println("Area of Circle: " + result);
+    public void show() {
+        System.out.println("Showing...");
     }
 }
 
-// Rectangle class implementing Shape
-class Rectangle implements Shape {
-    double length, width;
-
-    Rectangle(double length, double width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    public void area() {
-        double result = length * width;
-        System.out.println("Area of Rectangle: " + result);
-    }
-}
-
-// Main class
 public class Interface {
     public static void main(String[] args) {
-        Shape s1 = new Circle(5);
-        Shape s2 = new Rectangle(4, 6);
-
-        s1.area();
-        s2.area();
+        Demo d = new Demo();
+        d.print();
+        d.show();
     }
 }
